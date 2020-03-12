085afea1a6112322ac497fc6b3e6e6b9ddf43c8f
Anderson <anderson@datapraxis.com>
2020-03-12 11:53:02 -0300

    Updates

---
+++ precache-manifest.3200476b1dd8d07d5b2edba8ddf487a0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90daca7573ff9de9c5b98a741d9cad1b",
+    "url": "/mentoria2020/index.html"
+  },
+  {
+    "revision": "3ebf65448143b7e25ec2",
+    "url": "/mentoria2020/static/css/main.14aaca9a.chunk.css"
+  },
+  {
+    "revision": "b7797f840978dbbc7fd2",
+    "url": "/mentoria2020/static/js/2.8afb4bdd.chunk.js"
+  },
+  {
+    "revision": "3ebf65448143b7e25ec2",
+    "url": "/mentoria2020/static/js/main.edbb81a5.chunk.js"
+  },
+  {
+    "revision": "f4954951a2608696fa0c",
+    "url": "/mentoria2020/static/js/runtime~main.b6db723b.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/mentoria2020/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/mentoria2020/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/mentoria2020/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mentoria2020/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/mentoria2020/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/mentoria2020/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/mentoria2020/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/mentoria2020/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/mentoria2020/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "78d7aa036ad7cac4db34e05b7ccc153a",
+    "url": "/mentoria2020/static/media/image_bg.78d7aa03.jpg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/mentoria2020/static/media/roboto-700.e07df86c.ttf"
+  }
+]);
